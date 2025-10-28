b1ca8880158e5674e553f3b36abb5c9793d58d7a
DiskKun <luca5h1905@gmail.com>
2025-10-28 14:27:29 -0400

    basic monster added

---
+++ Assets/Scripts/Monster.cs
@@ -0,0 +1,25 @@
+using UnityEngine;
+
+public class Monster : MonoBehaviour
+{
+    public Transform target;
+    public float chaseSpeed;
+    Rigidbody2D rb;
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        rb = GetComponent<Rigidbody2D>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    private void FixedUpdate()
+    {
+        Vector2 direction = (target.position - transform.position).normalized;
+        rb.MovePosition(rb.position + direction * Time.fixedDeltaTime * chaseSpeed);
+    }
+}
